c8fd2d53ab3608d147081d5b30be08e9ca5af512
Sergio Fernández <sergio@wikier.org>
2006-09-24 08:42:14 +0000

    new uri for swaml ontology

--- swaml/classes/namespaces.py
+++ swaml/classes/namespaces.py
@@ -2,7 +2,7 @@
 
 from rdflib import Namespace
 
-SWAML = Namespace(u'http://swaml.berlios.de/ns/swaml.owl#')
+SWAML = Namespace(u'http://swaml.berlios.de/ns/0.1#')
 RDF = Namespace(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
 RDFS = Namespace(u'http://www.w3.org/2000/01/rdf-schema#')
 DC = Namespace(u'http://purl.org/dc/elements/1.1/')

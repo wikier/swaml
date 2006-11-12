f566290a1ec0b8b8ba4db44d666b7033d39ff932
Sergio Fernández <sergio@wikier.org>
2006-11-12 11:21:19 +0000

    more FOAFs added

--- swaml/classes/services.py
+++ swaml/classes/services.py
@@ -79,7 +79,13 @@ class FoafUtils:
                      '84d076726727b596b08198e26ef37e4817353e97' : 'http://frade.no-ip.info:2080/~ivan/foaf.rdf',
                      '3665f4f2370ddd6358da4062f3293f6dc7f39b7c' : 'http://eikeon.com/foaf.rdf',
                      '56e6f2903933a611708ebac456d45e454ddb8838' : 'http://captsolo.net/semweb/foaf-captsolo.rdf',
-                     '9a6b7eefc08fd755d51dd9321aecfcc87992e9a2' : 'http://www.johnbreslin.com/foaf/foaf.rdf'
+                     '42ec6894d9a48b5647279e866a0643eb7caded36' : 'http://captsolo.net/semweb/foaf-captsolo.rdf',
+                     '9a6b7eefc08fd755d51dd9321aecfcc87992e9a2' : 'http://www.johnbreslin.com/foaf/foaf.rdf',
+                     '36cf5b9757bdc1529831c210dbd81961472f1eb0' : 'http://platon.escet.urjc.es/~axel/foaf.rdf',
+                     '80248cbb1109104d97aae884138a6afcda688bd2' : 'http://apassant.net/foaf.rdf',
+                     '669fe353dbef63d12ba11f69ace8acbec1ac8b17' : 'http://dannyayers.com/misc/foaf/foaf.rdf',
+                     '349f4bf50f11185d3503b14f1a6ccfc425116b12' : 'http://www.openlinksw.com/dataspace/kidehen@openlinksw.com/about.rdf',
+                     'f67ba8825fc92f3db74ae725491c7c224287a367' : 'http://www.talkdigger.com/foaf/fgiasson'
                 }
                 
         if (mail_sha1sum in foafs):

293fca4e58258f88695f6f87fe3a2d0e60f79cd9
Sergio Fernández <sergio@wikier.org>
2006-12-09 16:31:58 +0000

    XHTML 1.1

--- webpage/index.php
+++ webpage/index.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
     <title>SWAML - Semantic Web Archive of Mailing Lists</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="robots" content="index,follow" />
     <meta name="keywords" content="SWAML, semantic web, mailing lists, python, sioc" />
     <meta name="language" content="en" />

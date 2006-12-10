7bcf15acbd188ab017ff9462bfb5f622f2898774
Sergio Fernández <sergio@wikier.org>
2006-12-10 22:45:43 +0000

    minor details fixed at project description

--- webpage/index.php
+++ webpage/index.php
@@ -35,11 +35,14 @@
 
     <div id="content">
 	  <p>
-		<acronym title="Semantic Web Archive of Mailing Lists">SWAML</acronym>, pronounced <em>[sw&aelig;ml]</em>,
-		is  a research project around the semantic web technologies to publish the mailing lists&acute;s 
-		archive into a <acronym title="Resource Description Framework">RDF</acronym> format, developed at 
-		<a href="http://www.euitio.uniovi.es/">University of Oviedo</a> (Spain). You can visit the 
-		<a href="http://developer.berlios.de/projects/swaml/">project page at BerliOS</a> for more details.
+		<acronym title="Semantic Web Archive of Mailing Lists">SWAML</acronym>, pronounced 
+		<em>[sw&aelig;ml]</em>, is  a research project around the semantic web technologies 
+		to publish the mailing lists' archives into a 
+		<acronym title="Resource Description Framework">RDF</acronym> format. It has been
+		developed at <a href="http://www.euitio.uniovi.es/">University of Oviedo</a> (Spain). 
+		You can visit the 
+		<a href="http://developer.berlios.de/projects/swaml/">project page at BerliOS</a> 
+		for more details.
 	  </p>
 
       <img src="images/swaml.png" width="400" height="336" alt="SWAML process description" id="process" />

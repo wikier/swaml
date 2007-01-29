ead3097ff3f2a58cb174faef6e15abe3635cb61e
Sergio Fernández <sergio@wikier.org>
2007-01-29 07:59:08 +0000

    using the new short link for news in the script that parses the rss

--- webpage/functions.php
+++ webpage/functions.php
@@ -75,17 +75,18 @@ class SWAML {
 
 	    		foreach ($items as $item) {
 	      			$title = $item[title];
-	      			$link  = $item[link];
-	      			$parted = explode('=', $link);
-	      			if ($parted[1]!=22514 && $parted[1]!=22842 && $parted[1]!=22843) {
-	      			 $description = $item[description];
-	      			 $description = $this->acronyms_replace($description);
-	      			 $description = $this->make_clickable($description);
-	      			 $pubDate  = $item[pubdate];
-	      			 $date = explode(" ", $pubDate);
-	      			 $ret .= '<dt>['.$date[1].'-'.$months[$date[2]].'-'.$date[3].'] ';
-              			 $ret .= '<a href="'.$link.'"><strong>'.$title.'</strong></a></dt>';
-	      			 $ret .= '<dd>'.$description.'</dd>';
+	      			$parted = explode('=', $item[link]);
+	      			$id = $parted[1];
+	      			if ($id!=22514 && $id!=22842 && $id!=22843) {
+	      			  $link  = 'http://swaml.berlios.de/news/' . $id;
+	      			  $description = $item[description];
+	      			  $description = $this->acronyms_replace($description);
+	      			  $description = $this->make_clickable($description);
+	      			  $pubDate  = $item[pubdate];
+	      			  $date = explode(" ", $pubDate);
+	      			  $ret .= '<dt>['.$date[1].'-'.$months[$date[2]].'-'.$date[3].'] ';
+	      			  $ret .= '<a href="'.$link.'"><strong>'.$title.'</strong></a></dt>';
+	      			  $ret .= '<dd>'.$description.'</dd>';
 	      			}
 	    		}
 

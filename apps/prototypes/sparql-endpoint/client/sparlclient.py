d7212ad87d585598bd2e024b30a72c06e4a6e978
Sergio Fernández <sergio@wikier.org>
2007-07-06 08:54:37 +0000

    added gpl headers

--- apps/prototypes/sparql-endpoint/client/sparlclient.py
+++ apps/prototypes/sparql-endpoint/client/sparlclient.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python2.4
+# SWAML <http://swaml.berlios.de/>
+# Semantic Web Archive of Mailing Lists
+#
+# Copyright (C) 2007 Sergio Fdez, Diego Berrueta
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
 
 import sys
 import urllib, urllib2

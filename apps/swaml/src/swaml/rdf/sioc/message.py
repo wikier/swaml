a92e146212aa44464d22e0c7e3381c6908a99f4f
Sergio Fernández <sergio@wikier.org>
2007-07-22 17:47:51 +0000

    fixed credits

--- apps/swaml/src/swaml/rdf/sioc/message.py
+++ apps/swaml/src/swaml/rdf/sioc/message.py
@@ -3,7 +3,7 @@
 # SWAML <http://swaml.berlios.de/>
 # Semantic Web Archive of Mailing Lists
 #
-# Copyright (C) 2005-2007 Sergio Fernández
+# Copyright (C) 2005-2007 Sergio Fernández, Diego Berrueta
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by the

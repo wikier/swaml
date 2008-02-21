c4020f6599389a0d1b11d98af368f83f8ade8f99
Sergio Fernández <sergio@wikier.org>
2008-02-21 11:48:33 +0000

    reindented to files

--- apps/buxon/src/buxon/rdf/cache.py
+++ apps/buxon/src/buxon/rdf/cache.py
@@ -31,173 +31,173 @@ from buxon.common.date import MailDate
 
 class Cache:
 
-	def orderByDate(self, posts):
-		"""
- 		Order by date a list of posts
-		
-		@param posts: posts to order
-		@return: posts ordered
-		"""
-		
-		#SPARQL in RDFLib doesn't support 'ORDER BY' queries
-		#then we'll implement a rustic support to order by dates
-		#state: testing
-        
-		#extract dates in integer long format
-		dict = {}
-		dates = []
-		for (post, title, date, creator, content, parent) in posts:
-			intDate = MailDate(date).getInteger()
-			dates.append(intDate)
-			dict[intDate] = (post, title, date, creator, content, parent)
-            
-		#and we put ordered into a new list
-		dates.sort()
-		ordered = [] 
-		for date in dates:
-			ordered.append(dict[date])
-            
-		return ordered
-            
-	def filterPosts(self, posts, min=None, max=None, text=None):
-		"""
-		Filter post from some conditions
-        
-		@param posts: list of posts
- 		@param min: min date
-		@param max: max date
-		@param text: text to search
-		"""
-        
-		filtered = []
-        
-		for (post, title, date, creator, content, parent) in posts:
-            
-			intDate = MailDate(date).getInteger()
-            
-			#exist if date is bigger
-			if (max!=None and intDate>max):
-				break
-            
-			#continue if is smaller
-			if  (min!=None and intDate<min):
-				continue
-            
-			#and then filter by text
-			if (text == None):
-				filtered.append((post, title, date, creator, content, parent))
- 			else:
- 				if (self.__like(title,text) or self.__like(content,text)):
-					filtered.append((post, title, date, creator, content, parent))
-            
- 		return filtered
+    def orderByDate(self, posts):
+        """
+        Order by date a list of posts
 
-	def __like(self, text, query):
-		"""
+        @param posts: posts to order
+        @return: posts ordered
+        """
+
+        #SPARQL in RDFLib doesn't support 'ORDER BY' queries
+        #then we'll implement a rustic support to order by dates
+        #state: testing
+
+        #extract dates in integer long format
+        dict = {}
+        dates = []
+        for (post, title, date, creator, content, parent) in posts:
+            intDate = MailDate(date).getInteger()
+            dates.append(intDate)
+            dict[intDate] = (post, title, date, creator, content, parent)
+
+        #and we put ordered into a new list
+        dates.sort()
+        ordered = []
+        for date in dates:
+            ordered.append(dict[date])
+
+        return ordered
+
+    def filterPosts(self, posts, min=None, max=None, text=None):
+        """
+        Filter post from some conditions
+
+        @param posts: list of posts
+        @param min: min date
+        @param max: max date
+        @param text: text to search
+        """
+
+        filtered = []
+
+        for (post, title, date, creator, content, parent) in posts:
+
+            intDate = MailDate(date).getInteger()
+
+            #exist if date is bigger
+            if (max!=None and intDate>max):
+                break
+
+            #continue if is smaller
+            if  (min!=None and intDate<min):
+                continue
+
+            #and then filter by text
+            if (text == None):
+                filtered.append((post, title, date, creator, content, parent))
+            else:
+                if (self.__like(title,text) or self.__like(content,text)):
+                    filtered.append((post, title, date, creator, content, parent))
+
+        return filtered
+
+    def __like(self, text, query):
+        """
         Search words into a text
-        
+
         @param text: text where we'll search
         @param query: words to search
-		"""
-        
- 		text = text.lower()
-		query = query.lower().split(' ')
-        
-		for one in query:
- 			if not one in text:
- 				return False
-        
-		return True
+        """
 
-	def getValueForPredicate(self, subject, predicate):
-		"""
+        text = text.lower()
+        query = query.lower().split(' ')
+
+        for one in query:
+            if not one in text:
+                return False
+
+        return True
+
+    def getValueForPredicate(self, subject, predicate):
+        """
         Get value of a predicate
-        
+
         @param subject: subject
         @param predicate: predicate
-		"""        
-        
-		value = [x for x in self.graph.objects(URIRef(subject), predicate)]
-		if (len(value) > 0):
-			return value[0]
-		else:
-			return None
+        """
 
-	def query(self):
-		"""
-		Make a SPARQL query
-        
-		@return: posts result
-		"""
-        
- 		try:
-			sparqlGr = SPARQLGraph(self.graph)
-			select = ('?post', '?postTitle', '?date', '?userName', '?content', '?parent')            
-			where  = GraphPattern([('?post',    RDF['type'],            SIOC['Post']),
-                                          ('?post',    DC['title'],            '?postTitle'),
-                                          ('?post',    DCTERMS['created'],     '?date'),
-                                          ('?post',    SIOC['content'],        '?content'),
-                                          ('?post',    SIOC['has_creator'],    '?user'),
-                                          ('?user',    SIOC['name'],           '?userName')])
-			opt    = GraphPattern([('?post',    SIOC['reply_of'],       '?parent')])
-			posts = Query.query(sparqlGr, select, where, opt)
-			return self.orderByDate(posts)
-		except Exception, details:
-			print 'parsing exception:', str(details)
-			return None
-        
-	def getPostAuthor(self, post):
-		"""
+        value = [x for x in self.graph.objects(URIRef(subject), predicate)]
+        if (len(value) > 0):
+            return value[0]
+        else:
+            return None
+
+    def query(self):
+        """
+        Make a SPARQL query
+
+        @return: posts result
+        """
+
+        try:
+            sparqlGr = SPARQLGraph(self.graph)
+            select = ('?post', '?postTitle', '?date', '?userName', '?content', '?parent')
+            where  = GraphPattern([('?post',    RDF['type'],            SIOC['Post']),
+                              ('?post',    DC['title'],            '?postTitle'),
+                              ('?post',    DCTERMS['created'],     '?date'),
+                              ('?post',    SIOC['content'],        '?content'),
+                              ('?post',    SIOC['has_creator'],    '?user'),
+                              ('?user',    SIOC['name'],           '?userName')])
+            opt    = GraphPattern([('?post',    SIOC['reply_of'],       '?parent')])
+            posts = Query.query(sparqlGr, select, where, opt)
+            return self.orderByDate(posts)
+        except Exception, details:
+            print 'parsing exception:', str(details)
+            return None
+
+    def getPostAuthor(self, post):
+        """
         Get author of a post
-        
+
         @param post: post uri
-		"""
-        
-		authorUri = self.getValueForPredicate(post, SIOC['has_creator'])
-		author = self.getValueForPredicate(authorUri, SIOC['name'])
-		return author, authorUri
-        
-	def getPost(self, uri):
-		"""
+        """
+
+        authorUri = self.getValueForPredicate(post, SIOC['has_creator'])
+        author = self.getValueForPredicate(authorUri, SIOC['name'])
+        return author, authorUri
+
+    def getPost(self, uri):
+        """
         Get fields of a post
-        
+
         @param uri: post uri
         @return: post fields
- 		"""
-        
-		author, authorUri = self.getPostAuthor(uri)
-		listUri = self.getValueForPredicate(uri, SIOC['has_container'])
- 		listName = self.getValueForPredicate(listUri, DC['title'])
-		title = self.getValueForPredicate(uri, DC['title'])
-		date = self.getValueForPredicate(uri, DCTERMS['created'])
-		content = self.getValueForPredicate(uri, SIOC['content'])
-		return author, authorUri, listName, listUri, title, date, content
+        """
 
-	def dump(self, path='cache.rdf'):
- 		"""
+        author, authorUri = self.getPostAuthor(uri)
+        listUri = self.getValueForPredicate(uri, SIOC['has_container'])
+        listName = self.getValueForPredicate(listUri, DC['title'])
+        title = self.getValueForPredicate(uri, DC['title'])
+        date = self.getValueForPredicate(uri, DCTERMS['created'])
+        content = self.getValueForPredicate(uri, SIOC['content'])
+        return author, authorUri, listName, listUri, title, date, content
+
+    def dump(self, path='cache.rdf'):
+        """
         Dump graph on disk
-        
-        @param path: path to dump
- 		"""
-        
-		if bool(self.graph):
-			try:
-				file = open(path, 'w+')
- 				self.graph.serialize(destination=file, format="pretty-xml")
-				file.flush()
-				file.close()
- 			except IOError, detail:
-				print 'Error dumping cache: ' + str(detail)
 
-	def __init__(self, uri, ping, pb=None):
-		"""
-		Cache constructor
-		
-		@param uri: uri to load
-		@param pb: progress bar
-		"""
-		
-		self.uri = uri
-		fetcher = Fetcher(uri, ping, pb)
-		self.graph = fetcher.getData()
+        @param path: path to dump
+        """
+
+        if bool(self.graph):
+            try:
+                file = open(path, 'w+')
+                self.graph.serialize(destination=file, format="pretty-xml")
+                file.flush()
+                file.close()
+            except IOError, detail:
+                print 'Error dumping cache: ' + str(detail)
+
+    def __init__(self, uri, ping, pb=None):
+        """
+        Cache constructor
+
+        @param uri: uri to load
+        @param pb: progress bar
+        """
+
+        self.uri = uri
+        fetcher = Fetcher(uri, ping, pb)
+        self.graph = fetcher.getData()
 
--- apps/buxon/src/buxon/rdf/fetcher.py
+++ apps/buxon/src/buxon/rdf/fetcher.py
@@ -31,173 +31,173 @@ import socket
 import gtk
 
 class Fetcher:
-        
-	def __listPosts(self):
-		"""
-        List post at cache
-		"""
-        
-		try:    
-			sparqlGr = SPARQLGraph(self.graph)
-			select = ('?post', '?title')            
-			where  = GraphPattern([('?post', RDF['type'],   SIOC['Post']),
-                                          ('?post', DC['title'], '?title')])
-			posts = Query.query(sparqlGr, select, where)
-            
- 			print len(posts), 'posts:'
-            
-			for post, title in posts:
-				print post, 
-				try:
-					print title
-				except:
-					print '(bad formed title)'
-                
-		except Exception, details:
- 			print 'parsing exception:', str(details)
-			return None
-	
-    
-	def loadMailingList(self, uri):
-		"""
-        Load a mailing list into a graph memory
-        
-        @param uri: mailing list's uri
-		"""
-        
-		graph = ConjunctiveGraph()
-		print 'Getting mailing list data (', uri, ')...',
-		graph.parse(uri)
-		print 'OK, loaded', len(graph), 'triples'
-        
-		self.uri = self.__getForums(graph)[0]
-		print 'Using ' + self.uri + ' sioc:Forum'
-        
- 		if (self.pb != None):
- 			self.pb.progress()
-            
-		if (self.ptsw != None):
-			self.ptsw.ping(uri)
-        
-		return graph
-    
-	def __loadData(self, uri):
-		"""
-		Load data
-        
-		@param uri: uri to load
-		"""
-        
- 		print 'Resolving reference to get additional data (', uri, ')...',
-		try:
-			self.graph.parse(uri)
-		except:
-			print '\nAn exception ocurred parsing ' + uri
-			return
-        
-		if (self.pb != None):
- 			self.pb.progress()
-			while gtk.events_pending():
-				gtk.main_iteration()
-        
-		if (self.ptsw != None):
-			self.ptsw.ping(uri)
-                
-		print 'OK, now', len(self.graph), 'triples'    
-        
-	def __getForums(self, graph): 
-		"""
-        Get all sioc:Forum's in a graph
-		"""
-        
-		sparqlGr = SPARQLGraph(graph)
-		select = ('?uri')
-		where  = GraphPattern([('?uri', RDF['type'], SIOC['Forum'])])
-		forums = Query.query(sparqlGr, select, where)
-		return forums;
-        
-    
-	def loadAdditionalData(self):
-		"""
-        Load additional data of a mailing list
-		"""
-        
-		for post in self.graph.objects(self.uri, SIOC['container_of']):
-			if not self.hasValueForPredicate(post, SIOC['id']):
-				postSeeAlso = self.getValueForPredicate(post, RDFS['seeAlso'])
-				if (postSeeAlso == None):
-					self.__loadData(post)
-				else:
-					self.__loadData(postSeeAlso)
-        
- 		for user in self.graph.objects(predicate=SIOC['has_subscriber']):
-			if not self.hasValueForPredicate(user, SIOC['email_sha1']):
- 				self.__loadData(user)
 
-	def hasValueForPredicate(self, subject, predicate):
-		"""
-		Get if a predicate exists
-        
- 		@param subject: subject
- 		@param predicate: predicate
-		"""
-        
-		return (len([x for x in self.graph.objects(URIRef(subject), predicate)]) > 0)        
-       
-	def getValueForPredicate(self, subject, predicate):
-		"""
-        Get value of a predicate
-        
+    def __listPosts(self):
+        """
+        List post at cache
+        """
+
+        try:
+            sparqlGr = SPARQLGraph(self.graph)
+            select = ('?post', '?title')
+            where  = GraphPattern([('?post', RDF['type'],   SIOC['Post']),
+                              ('?post', DC['title'], '?title')])
+            posts = Query.query(sparqlGr, select, where)
+
+            print len(posts), 'posts:'
+
+            for post, title in posts:
+                print post,
+                try:
+                    print title
+                except:
+                    print '(bad formed title)'
+
+        except Exception, details:
+            print 'parsing exception:', str(details)
+            return None
+
+
+    def loadMailingList(self, uri):
+        """
+        Load a mailing list into a graph memory
+
+        @param uri: mailing list's uri
+        """
+
+        graph = ConjunctiveGraph()
+        print 'Getting mailing list data (', uri, ')...',
+        graph.parse(uri)
+        print 'OK, loaded', len(graph), 'triples'
+
+        self.uri = self.__getForums(graph)[0]
+        print 'Using ' + self.uri + ' sioc:Forum'
+
+        if (self.pb != None):
+            self.pb.progress()
+
+        if (self.ptsw != None):
+            self.ptsw.ping(uri)
+
+        return graph
+
+    def __loadData(self, uri):
+        """
+        Load data
+
+        @param uri: uri to load
+        """
+
+        print 'Resolving reference to get additional data (', uri, ')...',
+        try:
+            self.graph.parse(uri)
+        except:
+            print '\nAn exception ocurred parsing ' + uri
+            return
+
+        if (self.pb != None):
+            self.pb.progress()
+            while gtk.events_pending():
+                gtk.main_iteration()
+
+        if (self.ptsw != None):
+            self.ptsw.ping(uri)
+
+        print 'OK, now', len(self.graph), 'triples'
+
+    def __getForums(self, graph):
+        """
+        Get all sioc:Forum's in a graph
+        """
+
+        sparqlGr = SPARQLGraph(graph)
+        select = ('?uri')
+        where  = GraphPattern([('?uri', RDF['type'], SIOC['Forum'])])
+        forums = Query.query(sparqlGr, select, where)
+        return forums;
+
+
+    def loadAdditionalData(self):
+        """
+        Load additional data of a mailing list
+        """
+
+        for post in self.graph.objects(self.uri, SIOC['container_of']):
+            if not self.hasValueForPredicate(post, SIOC['id']):
+                postSeeAlso = self.getValueForPredicate(post, RDFS['seeAlso'])
+                if (postSeeAlso == None):
+                    self.__loadData(post)
+                else:
+                    self.__loadData(postSeeAlso)
+
+        for user in self.graph.objects(predicate=SIOC['has_subscriber']):
+            if not self.hasValueForPredicate(user, SIOC['email_sha1']):
+                self.__loadData(user)
+
+    def hasValueForPredicate(self, subject, predicate):
+        """
+        Get if a predicate exists
+
         @param subject: subject
         @param predicate: predicate
-		"""        
-        
-		value = [x for x in self.graph.objects(URIRef(subject), predicate)]
-		if (len(value) > 0):
-			return value[0]
-		else:
-			return None
-
-	def getData(self):
-        
-		try:
-			self.graph = self.loadMailingList(self.uri)
-		except Exception, details:
- 			print '\nAn exception ocurred parsing ' + self.uri + ': ' + str(details)
-			self.bad = True
- 			return
-        
-		self.loadAdditionalData()
-        
-		#self.__listPosts()
-        
-		if (self.pb != None):
-			self.pb.destroy()
-        
-		if (self.ptsw != None):
-			print self.ptsw.stats()
-
-		if self.bad:
-			return None
-		else:
-			print 'Total triples loaded:', len(self.graph)
-			return self.graph
-
-	def __init__(self, base, ping, pb=None):
-		"""
-		Cache constructor
-		
-		@param base: base uri to load
-		@param pb: progress bar
         """
-		
-		self.uri = base
-		self.graph = None
-		self.bad = False
-		self.pb = pb
-		self.ptsw = None
-		if ping:
-			self.ptsw = PTSW()
 
-		socket.setdefaulttimeout(5)
+        return (len([x for x in self.graph.objects(URIRef(subject), predicate)]) > 0)
+
+    def getValueForPredicate(self, subject, predicate):
+        """
+        Get value of a predicate
+
+        @param subject: subject
+        @param predicate: predicate
+        """
+
+        value = [x for x in self.graph.objects(URIRef(subject), predicate)]
+        if (len(value) > 0):
+            return value[0]
+        else:
+            return None
+
+    def getData(self):
+
+        try:
+            self.graph = self.loadMailingList(self.uri)
+        except Exception, details:
+            print '\nAn exception ocurred parsing ' + self.uri + ': ' + str(details)
+            self.bad = True
+            return
+
+        self.loadAdditionalData()
+
+        #self.__listPosts()
+
+        if (self.pb != None):
+            self.pb.destroy()
+
+        if (self.ptsw != None):
+            print self.ptsw.stats()
+
+        if self.bad:
+            return None
+        else:
+            print 'Total triples loaded:', len(self.graph)
+            return self.graph
+
+    def __init__(self, base, ping, pb=None):
+        """
+        Cache constructor
+
+        @param base: base uri to load
+        @param pb: progress bar
+        """
+
+        self.uri = base
+        self.graph = None
+        self.bad = False
+        self.pb = pb
+        self.ptsw = None
+        if ping:
+            self.ptsw = PTSW()
+
+        socket.setdefaulttimeout(5)
 

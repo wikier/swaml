2cef506894c9b9aaf15c4887a9483f6c76c3db4b
Sergio Fernández <sergio@wikier.org>
2008-03-05 16:44:52 +0000

    updated python meta info

--- apps/buxon/__init__.py
+++ apps/buxon/__init__.py
@@ -3,7 +3,7 @@
 # SWAML <http://swaml.berlios.de/>
 # Semantic Web Archive of Mailing Lists
 #
-# Copyright (C) 2006 Sergio Fdez, Diego Berrueta
+# Copyright (C) 2006-2008 Sergio Fernández, Diego Berrueta
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by the
@@ -15,10 +15,10 @@
 # or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 # for more details.
 
-__author__       = 'Sergio Fdez <http://www.wikier.org/>'
+__author__       = 'Sergio Fernández <http://www.wikier.org/>'
 __contributors__ = ['Diego Berrueta <http://www.berrueta.net/>',
                     'Jose Emilio Labra <http://www.di.uniovi.es/~labra/>']
-__copyright__    = 'Copyright 2005-2006, Sergio Fdez'
+__copyright__    = 'Copyright 2005-2008, Sergio Fernández'
 __license__      = 'GNU General Public License'
 __version__      = '0.0.4'
 __url__          = 'http://swaml.berlios.de/'

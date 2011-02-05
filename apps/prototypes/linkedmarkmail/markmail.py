9dd03bc964f1d0445ff404cce65f0381a3f02906
Sergio Fernández <sergio@wikier.org>
2011-02-05 10:23:09 +0000

    api skel implementation

---
+++ apps/prototypes/linkedmarkmail/markmail.py
@@ -0,0 +1,65 @@
+# -*- coding: utf8 -*-
+
+# LinkedMarkMail, an RDFizer for Mark Mail 
+#
+# Copyright (C) 2011 Sergio Fernández
+#
+# This file is part of SWAML <http://swaml.berlios.de/>
+# 
+# Foobar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+A simple python client for Mark Mail hacky API
+
+Futher details at: http://pastebin.com/M5NnyEZ8
+"""
+
+import urllib
+import urllib2
+import simplejson
+import warnings
+
+class MarkMail:
+
+    def __init__(self, base="http://markmail.org"):
+        self.base = base
+
+    def search(self, query, page=1 mode="json"):
+        uri = "%s/results.xqy?q=%s&page=%d&mode=%s" % (base, query, page, mode)
+        warnings.warn("This method is still unimplemented")
+        return None
+
+    def get_message(self, key, mode="json"):
+        uri = "%s/message.xqy?id=%s&mode=%s" % (self.base, key, mode)
+
+    def get_thread(self, key, mode="json"):
+        uri = "%s/thread.xqy?id=%s&mode=%s" % (self.base, key, mode)
+
+    def __request(self, uri, accept="application/json"):
+        """
+        Generic HTTP request
+       
+        @param uri: uri to request
+        @return: response
+        @rtype: file-like object
+        """
+       
+        headers = {
+                    "User-Agent" : "swaml (http://swaml.berlios.de/; sergio@wikier.org)",
+                    "Accept"     : accept
+                  }
+        request = urllib2.Request(uri, headers=headers)
+        return urllib2.urlopen(request)
+
